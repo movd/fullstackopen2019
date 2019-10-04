d7208563711c0ee83ffe475b735ece00127193cf
Moritz <22394314+movd@users.noreply.github.com>
2019-10-04 17:48:16 +0200

    Finished Ex 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  };
 
   const Header = props => {
     return <h1>{props.course}</h1>;
@@ -17,9 +23,9 @@ const App = () => {
   const Content = props => {
     return (
       <div>
-        <Part partName={part1} nExercises={exercises1}></Part>
-        <Part partName={part2} nExercises={exercises2}></Part>
-        <Part partName={part3} nExercises={exercises3}></Part>
+        <Part partName={part1.name} nExercises={part1.exercises} />
+        <Part partName={part2.name} nExercises={part2.exercises} />
+        <Part partName={part3.name} nExercises={part3.exercises} />
       </div>
     );
   };
@@ -38,9 +44,11 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}></Header>
+      <Header course={course} />
       <Content />
-      <Total sumExercises={exercises1 + exercises2 + exercises3}></Total>
+      <Total
+        sumExercises={part1.exercises + part2.exercises + part3.exercises}
+      />
     </div>
   );
 };
