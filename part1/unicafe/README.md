56ff8e73e225acf31f09df5dcd29f31c65825def
Moritz <22394314+movd@users.noreply.github.com>
2019-10-05 18:23:27 +0200

    Finished Ex 1.6 and 1.7

---
+++ part1/unicafe/README.md
@@ -0,0 +1,3 @@
+# Answers for Exercises 1.6 to 1.11
+
+My answers for Exercises [1.6 to 1.11 of Full Stack Open 2019](https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps#exercises).
---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  let count = good + neutral + bad;
+  let total = 1 * good + 0 * neutral + -1 * bad;
+  let average = count && total / count;
+  let positive = count && (good / count) * 100;
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <div>
+        <h2>statistics</h2>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {count}</div>
+        <div>average {average}</div>
+        <div>positive {positive} %</div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
