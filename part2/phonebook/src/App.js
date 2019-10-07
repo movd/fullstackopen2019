246ff839dce5d1c6213dfcf100a99c4754dfee7e
Moritz <22394314+movd@users.noreply.github.com>
2019-10-07 14:13:26 +0200

    Finished Ex. 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" }
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const handleChange = e => setNewName(e.target.value);
+  const handleNameChange = e => setNewName(e.target.value);
+  const handleNumberChange = e => setNewNumber(e.target.value);
 
   const handleClick = e => {
     e.preventDefault();
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     };
 
     if (persons.some(p => p.name === newName)) {
@@ -19,14 +24,23 @@ const App = () => {
     }
   };
 
-  const renderPersons = persons.map(p => <div key={p.name}>{p.name}</div>);
+  const renderPersons = persons.map(p => (
+    <div key={p.name}>
+      {p.name} {p.number}
+    </div>
+  ));
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input type="text" value={newName} onChange={handleChange} />
+          name:{" "}
+          <input type="text" value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number:{" "}
+          <input type="text" value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button onClick={handleClick}>add</button>
